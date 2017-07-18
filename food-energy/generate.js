f544523f1a1d60e620e3aaf40b678a90d0e1738c
Taras Labiak <kissarat@gmail.com>
2017-07-18 12:57:46 +0300

    + food energy

---
+++ food-energy/generate.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+const template = fs.readFileSync(__dirname + '/template.html').toString('utf8')
+const data = require('./data')
+
+const strings = []
+for (const row of data) {
+  const cells = []
+  for (const cell of row) {
+    let value = cell
+    if ('number' === typeof cell) {
+      if (!Number.isInteger(cell)) {
+        const number = cell.toString().split('.')
+        value = `${number[0]}<span>,${number[1]}</span>`
+      }
+      else if (0 === cell) {
+        value = `<span>${cell}</span>`
+      }
+    }
+    const lower = 'string' === typeof cell ? cell.toLocaleLowerCase() : cell
+    cells.push(`<td title="${lower}">${value}</td>`)
+  }
+  const string = cells.join('')
+  strings.push(`<tr>${string}</tr>`)
+}
+
+const html = template.replace('{data}', strings.join('\n'))
+fs.writeFileSync(__dirname + '/index.html', html)
