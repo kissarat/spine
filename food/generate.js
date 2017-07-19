1e0457b066434c27a38c44708a71df01454b2300
Taras Labiak <kissarat@gmail.com>
2017-07-19 15:29:06 +0300

    + keywords

--- food/generate.js
+++ food/generate.js
@@ -3,9 +3,10 @@ const fs = require('fs')
 const template = fs.readFileSync(__dirname + '/template.html').toString('utf8')
 const data = require('./energy')
   .sort((a, b) => a[0].localeCompare(b[0]))
-  // .sort((a, b) => b[4] - a[4])
+// .sort((a, b) => b[4] - a[4])
 
 const strings = []
+const qt = {}
 for (const row of data) {
   const cells = []
   for (const cell of row.slice(0, 5)) {
@@ -19,16 +20,35 @@ for (const row of data) {
         value = `<span>${cell}</span>`
       }
     }
-    const lower = 'string' === typeof cell ? cell.toLocaleLowerCase()
+    let lower = cell
+    if ('string' === typeof cell) {
+      lower = cell.toLocaleLowerCase()
         .replace(/[^[a-zа-яъыіїґ\s]+/igu, '')
         .replace(/\s+/g, ' ')
         .trim()
-      : cell
+      for (const key of lower.split(' ')) {
+        if (!(key in qt)) {
+          qt[key] = 0
+        }
+        qt[key]++
+      }
+    }
     cells.push(`<td title="${lower}">${value}</td>`)
   }
   const string = cells.join('')
   strings.push(`<tr>${string}</tr>`)
 }
 
-const html = template.replace('{data}', strings.join('\n'))
+const qz = []
+for (const key in qt) {
+  const v = qt[key]
+  if (key.length > 2 && !/\w/.test(key)) {
+    qz.push([key, v])
+  }
+}
+qz.sort((a, b) => b[1] - a[1])
+
+const html = template
+  .replace('{data}', strings.join('\n'))
+  .replace('"dict"', JSON.stringify(qz.map(a => a[0]).slice(0, 1000)))
 fs.writeFileSync(__dirname + '/energy/index.html', html)
